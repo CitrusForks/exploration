f41f4e6c2eb23dca9586dbd6e77c5653118897e5
Greg V <hmaon@bumba.net>
2013-06-05 23:30:30 -0400

    so tired...

--- dxgame/Actor.cpp
+++ dxgame/Actor.cpp
@@ -11,11 +11,13 @@ using namespace std;
 
 static bool arrrg = false;
 
-Actor::Actor(int modelRefNum) : m_modelRefNum(modelRefNum)
+Actor::Actor(int modelRefNum) : m_modelRefNum(modelRefNum), m_pitch(0.0f), m_yaw(0.0f)
 {
-    m_heading = XMVectorSet(0, 0, 1, 0);
+    //m_heading = XMVectorSet(0, 0, 1, 0);
+    //m_upHint = XMVectorSet(0,1,0,0);
+    
+    m_correction = XMMatrixIdentity(); // TODO
     m_position = XMVectorSet(0, 0, 0, 0);
-    m_upHint = XMVectorSet(0,1,0,0);
     updateWorldMatrix();
 
 #if 0
@@ -43,6 +45,7 @@ void Actor::moveTo( DirectX::FXMVECTOR to )
     updateWorldMatrix();
 }
 
+#if 0
 void Actor::updateWorldMatrix()
 {
     XMVECTOR up = XMVectorSet(0, 1, 0, 0); 
@@ -65,21 +68,43 @@ void Actor::updateWorldMatrix()
 
     m_world = XMLoadFloat4x4(&rotation) * XMMatrixTranslationFromVector(m_position);
 }
+#endif 
+// on second thought, let's just store yaw and pitch and stop over-thinking this
+// if we need a heading vector, we can generate it
+// although perhaps we should store a quaternion? but then we would either be continuously multiplying quaternions, which is a good way to accumulate error,
+// and/or we might have to transform the quaternion back into Euler-ish angles which is so boneheaded that DirectXMath doesn't even include it as a library function
+// here it is for reference: http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles -- not a great plan...
+
+void Actor::updateWorldMatrix()
+{
+    m_world = m_correction * XMMatrixRotationRollPitchYaw(m_pitch, m_yaw, 0) * XMMatrixTranslationFromVector(m_position);
+}
+
+
 
 void Actor::setPitchYaw( float pitch, float yaw )
 {
-    XMVECTOR quato = XMQuaternionRotationRollPitchYaw(pitch, yaw, 0);
-    m_heading = XMVector3Normalize(XMVector3Rotate(XMVectorSet(0, 0, 1, 0), quato));
-    if (arrrg) updateWorldMatrix();
-    else m_world = XMMatrixRotationQuaternion(quato) * XMMatrixTranslationFromVector(m_position);
+    m_pitch = pitch;
+    m_yaw = yaw;
+
+    //m_heading = XMVector3Normalize(XMVector3Rotate(XMVectorSet(0, 0, 1, 0), quato));
+    //if (arrrg) updateWorldMatrix();
+    //else m_world = XMMatrixRotationQuaternion(quato) * XMMatrixTranslationFromVector(m_position);
 
     arrrg = !arrrg;
 }
 
+#if 0
 void Actor::setHeading( DirectX::FXMVECTOR heading )
 {
     m_heading = heading;
     updateWorldMatrix();
+}
+#endif
+
+DirectX::XMVECTOR Actor::getHeading()
+{
+    return XMVector3Normalize(XMVector3Rotate(XMVectorSet(0, 0, 1, 0), XMQuaternionRotationRollPitchYaw(m_pitch, m_yaw, 0)));
 }
 
 
--- dxgame/Actor.h
+++ dxgame/Actor.h
@@ -8,8 +8,11 @@ __declspec(align(16)) class Actor
 {
 private:
     DirectX::XMVECTOR m_position;
-    DirectX::XMVECTOR m_heading, m_upHint;
+    //DirectX::XMVECTOR m_heading, m_upHint;
+    DirectX::XMMATRIX m_correction; // a constant correction to be applied first to the world matrix just in case--mostly because our "art assets" are actually "jacked up downloads from the free 3d model sites"
     DirectX::XMMATRIX m_world;
+
+    float m_pitch, m_yaw;
 
     int m_modelRefNum;
     
@@ -27,7 +30,11 @@ public:
     void move(DirectX::FXMVECTOR );
 
     void setPitchYaw(float pitch, float yaw);
-    void setHeading(DirectX::FXMVECTOR heading);
+    //void setHeading(DirectX::FXMVECTOR heading);
+
+    DirectX::XMVECTOR getHeading();
+    //void estimatePitchYaw(float &out_pitch, float &out_yaw);
+    void getPitchYaw(float &out_pitch, float &out_yaw) { out_pitch = m_pitch; out_yaw = m_yaw; }
 
     Actor(int modelRefNum);
     ~Actor(void);
