6bb48a1e3cf4d05b447e88ec904299d31ca5821f
Greg V <hmaon@bumba.net>
2013-06-12 01:55:22 -0400

    forgot files :|

---
+++ dxgame/SSE2Aligned.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <stdlib.h>
+#include "Errors.h"
+
+// this is a mix-in containing aligning overloads for operator new and operator delete
+
+__declspec (align(16)) class SSE2Aligned
+{
+public:
+    inline SSE2Aligned(void)
+    {
+    }
+
+    virtual ~SSE2Aligned(void)
+    {
+
+    }
+
+    void *operator new(size_t size)
+    {
+        return _aligned_malloc(size, 16);
+    }
+
+    void operator delete(void *p)
+    {
+        _aligned_free(p);
+    }
+};
+
